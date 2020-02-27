957b2eb5a7a389d05cb23d9794fb3521ee0f269a
Lucas Amado <amadolucas99@gmail.com>
2020-02-27 10:42:50 +0100

    service peticiones vacío

---
+++ SATapp/app/src/main/java/com/gonzaloandcompany/satapp/retrofit/ServicePeticiones.java
@@ -0,0 +1,10 @@
+package com.gonzaloandcompany.satapp.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface ServicePeticiones {
+
+}
