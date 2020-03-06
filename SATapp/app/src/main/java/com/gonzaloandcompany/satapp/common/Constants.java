49faf92c30faf49f9b861af302f0a2e09cebd15a
Esperanza Macarena <espemacem@gmail.com>
2020-03-06 12:36:01 +0100

    ya lista ticket creado por tecnico ademas de los asignados

--- SATapp/app/src/main/java/com/gonzaloandcompany/satapp/common/Constants.java
+++ SATapp/app/src/main/java/com/gonzaloandcompany/satapp/common/Constants.java
@@ -2,7 +2,7 @@ package com.gonzaloandcompany.satapp.common;
 
 public class Constants {
     public static final String BASE_URL = "https://heroku-satapp.herokuapp.com";
-    public static final String TOKEN_PROVISIONAL = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlNTYzZjM1YTc5MjczNjQxYzNmOGFmNCIsImlhdCI6MTU4MzQyODk5NX0.JxsiuxFYQD7fYVnWonOHgmyLjrtQbFNVd1lz494q5H4";
+    public static final String TOKEN_PROVISIONAL = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlNTk0Mjc4YTFmYzFiMDAxN2U5NjZjNSIsImlhdCI6MTU4MzQ5MzE3NX0.A9VW8VRGlm3yMr6e0qjwinwHbD7GgWt4hZCdxjPC4NI";
     public static final String ID_INVENTARIABLE = "ID_INVENTARIABLE";
     public static final String RESULT_QR = "RESULT_QR";
     public static final int CODIGO_INTENT = 2;
